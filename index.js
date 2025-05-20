b8a2835b716e90460bab3832a02605afa14fd22c
Igor Oliveira <Igor Pc Pessoal>
2025-05-20 19:48:44 -0300

    Fix2

--- index.js
+++ index.js
@@ -55,12 +55,12 @@ app.get("/vehicles", (req, res) => {
       },
       {
         id: 2,
-        vehicle: "Mustang",
+        vehicle: "Ranger Storm",
         vin: "2RFAASDY54E4HDU34874",
         volumetotal: 1500,
         connected: 500,
         softwareUpdates: 750,
-        img: "http://localhost:3001/img/mustang.png",
+        img: "http://localhost:3001/img/storm.png",
       },
       {
         id: 3,
@@ -80,15 +80,7 @@ app.get("/vehicles", (req, res) => {
         softwareUpdates: 2050,
         img: "http://localhost:3001/img/broncoSport.png",
       },
-      {
-        id: 7,
-        vehicle: "Ranger Storm",
-        vin: "2FRHDUYS2Y63NHD22654",
-        volumetotal: 145750,
-        connected: 70000,
-        softwareUpdates: 27020,
-        img: "http://localhost:3001/img/storm.jpg",
-      }
+      
       
     ];
 
@@ -123,7 +115,7 @@ app.post("/vehicleData", (req, res) => {
       case "2RFAASDY54E4HDU34874":
         return res.status(200).json({
           id: 2,
-          vehicle: "Mustang",
+          vehicle: "Ranger Storm",
           odometro: 130000,
           nivelCombustivel: 19,
           status: "off",
@@ -132,7 +124,7 @@ app.post("/vehicleData", (req, res) => {
           volumetotal: 1500,
           connected: 500,
           softwareUpdates: 750,
-          img: "https://api-ford-enter.onrender.com/img/mustang.png",
+          img: "https://api-ford-enter.onrender.com/img/storm.png",
         });
 
       case "2FRHDUYS2Y63NHD22455":
@@ -165,16 +157,6 @@ app.post("/vehicleData", (req, res) => {
           img: "https://api-ford-enter.onrender.com/img/broncoSport.png",
         });
 
-        case "2FRHDUYS2Y63NHD22456":
-          return res.status(200).json({
-            id: 7,
-            vehicle: "Ranger Storm",
-            volumetotal: 145750,
-            connected: 70000,
-            softwareUpdates: 27020,
-            img: "https://api-ford-enter.onrender.com/img/storm.jpg",
-
-          })
 
       case "2FRHDUYS2Y63NHD22654":
         return res.status(200).json({
